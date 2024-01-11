45c0fa14f6d6b9f982cfe0aafacec605273addef
sridharvs <sridharvsvs@gmail.com>
2024-01-11 11:47:35 +0530

    flutterfire_cli connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_textmsg_fb_app/auth/login_or_register_page.dart';
+import 'package:flutter_textmsg_fb_app/firebase_options.dart';
 import 'package:flutter_textmsg_fb_app/pages/login_page.dart';
 import 'package:flutter_textmsg_fb_app/pages/register_page.dart';
 import 'package:flutter_textmsg_fb_app/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
